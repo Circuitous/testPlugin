d202ea8d4684e3a29364ea5bc25e8bd0f2f7c109
Circuitous <slimecube93@gmail.com>
2015-04-28 13:19:02 +0200

    Initial commit

---
+++ src/testTube/TestPlugin.java
@@ -0,0 +1,30 @@
+package testTube;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+/**
+ * Created by Const on 28.04.2015.
+ */
+public class TestPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Plugin works just fine.");
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void loginMessage(PlayerLoginEvent event){
+        getLogger().log(Level.INFO, "Player " + event.getPlayer().getName() + " is joining.");
+    }
+}
